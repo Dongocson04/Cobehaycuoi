1d375a36c0a928437045830570fa8058ce3d2ee9
Dongocson04 <92036713+Dongocson04@users.noreply.github.com>
2023-05-14 09:56:49 +0700

    Initialize patch-1

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Tiêu đề web",
+    introTitle: 'Tên người yêu bạn',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu không yêu thì cút<33333333',
+    mess: 'cút',
+    messDesc: 'nghỉ chơi.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://github.com/ngocson' 
