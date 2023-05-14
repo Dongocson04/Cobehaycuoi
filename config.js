c2206131e45d161eac0c64bcd33090faa55bbf37
Dongocson04 <92036713+Dongocson04@users.noreply.github.com>
2023-05-14 09:56:48 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "Hi cậu <3",
+    introTitle: 'không có ai đâu',
+    introDesc: `Dường như ta còn vương vẫn mãi
+    Những kỉ niệm mà bạn bên tôi 
+    Ta thật lòng nuối tiếc thời gian trôi
+    Vì giờ đây chỉ còn là kỉ niệm `,
+    btnIntro: '^^HiHi^^',
+    title: 'Có phải cậu cũng giống như tớ 🥰',
+    desc: 'Phải chăng cậu đã thích tớ đúng không ? ',
+    btnYes: 'Thích lắm <3',
+    btnNo: 'Không nha :3',
+    question: 'Có biết bao nhiêu người sao cậu lại thích tớ  <3',
+    btnReply: 'Gửi cho cậu   <3',
+    reply: 'Không thích cũng phải thích  <3',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messLink: 'https://github.com/ngocson/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
